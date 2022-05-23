d637df6c15fa9dabca0a105fbd2acf838f3cd44f
Lasse96 <91594348+Lasse96@users.noreply.github.com>
2022-05-24 01:42:28 +0200

    ..

--- blog.js
+++ blog.js
@@ -1,24 +1,85 @@
-const output = document.querySelector(".blogoutput");
+const output = document.getElementById("blogoutput");
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 
 const id = params.get("id");
+if (id == null)
+{
+    const url = "https://lassestrand.no/eksamen1/wp-json/wp/v2/posts?_embed";
+
+
+    fetch(url)
+        .then (respons => respons.json())
+        .then (data => listPosts(data))
+        .catch ((error) => {
+            console.error('Error', error);
+        }
+    );
+
+
+    function listPosts(posts){
+        let list = "";
+        let button = "";
+        console.log(posts);
+        for (let post of posts) {
+            list += `
+            <div class="post">
+            <a href="./blog.html?id=${post.id}"><img src="${post._embedded['wp:featuredmedia'][0].source_url}" class="imgposts postimg"></a>
+            <h2 class="title lesom"><a href="./blog.html?id=${post.id}" class="decor">${post.title.rendered}</a></h2>
+            </div>`
+        }
+        button = `<div id="loadmore">
+        <button id="loadm" onclick="loadMore()">Load More</button>
+        </div>`
+        output.innerHTML = list + button;
+    }
+}
+else {
 
 const url = "https://lassestrand.no/eksamen1/wp-json/wp/v2/posts/" + id;
 
 fetch(url)
-.then (respons => respons.json())
-.then (data => listPosts(data))
-.catch ((error) => {
-    console.error('Error', error);
-});
+    .then (respons => respons.json())
+    .then (data => listPost(data))
+    .catch ((error) => {
+        console.error('Error', error);
+    }
+);
 
 
-function listPosts(post){
-    const title = post.title.rendered;
+    function listPost(post){
+        output.innerHTML = `
+            <h2>${post.title.rendered}</h2>
+            <a href="./blog.html?id=${post.id}">Les om ${post.title.rendered}</a>`
+    }
     
-    output.innerHTML += `
-        <a href= "${post.featured_media}"></a>
-        <h2>${post.title.rendered}</h2>
-        <a href="./blog.html?id=${post.id}">Les om ${post.title.rendered}</a>`
+}
+
+function loadMore() {
+    const url = "https://lassestrand.no/eksamen1/wp-json/wp/v2/posts?_embed&page=2";
+
+
+    fetch(url)
+        .then (respons => respons.json())
+        .then (data => listPosts(data))
+        .catch ((error) => {
+            console.error('Error', error);
+        }
+    );
+
+
+    function listPosts(posts){
+        let removeLoad = document.getElementById("loadmore");
+        output.removeChild(removeLoad);
+        let list = "";
+        console.log(posts);
+        for (let post of posts) {
+            list += `
+            <div class="post">
+            <a href="./blog.html?id=${post.id}"><img src="${post._embedded['wp:featuredmedia'][0].source_url}" class="imgposts postimg"></a>
+            <h2 class="title lesom"><a href="./blog.html?id=${post.id}" class="decor">${post.title.rendered}</a></h2>
+            </div>`
+        }
+        output.innerHTML += list;
+    }
 }
--- script.js
+++ script.js
@@ -23,20 +23,16 @@ function listPosts(posts){
     output.innerHTML = list;
 }
 
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
 carousel = document.getElementById("carousel")
 
-function lSlide() {
+function left() {
   carousel.scrollBy({
     top: 0,
     left: -1020,
     behavior: 'smooth'
   });
 }
-function rSlide() {
+function right() {
   carousel.scrollBy({
     top: 0,
     left: 1020,
@@ -44,76 +40,57 @@ function rSlide() {
   });
 }
 
+const submitted = document.querySelector("#submitted")
+const form = document.querySelector("#form");
 
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("posts");
-    if (n > slides.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+const yourName = document.querySelector("#name");
+const subject = document.querySelector("#subject");
+const email = document.querySelector("#email");
+const address = document.querySelector("#address");
+
+const nameAlert = document.querySelector("#nameAlert");
+const subjectAlert = document.querySelector("#subjectAlert");
+const emailAlert = document.querySelector("#emailAlert");
+const addressAlert = document.querySelector("#addressAlert");
+
+form.addEventListener("submit", runForm);
+
+function runForm(sub){
+    sub.preventDefault();
+
+    let inputName = yourName.value.trim();
+    nameAlert.innerHTML = "";
+    if (inputName.length < 2){
+        nameAlert.innerHTML += "This name is to short";
     }
-    slides[slideIndex-1].style.display = "block";  
-  }
+    if(/\d/.test(inputName)){
+        nameAlert.innerHTML += "Don't use digits";
+    }
 
-// const nav = document.querySelector("#nav");
+    let inputSubject = subject.value.trim();
+    subjectAlert.innerHTML = "";
+    if(inputSubject.length < 10){
+        subjectAlert.innerHTML += "Must be atleast 10 letters";
+    }
 
-// nav.innerHTML = `<ul>
-// <li><a href="index.html" class= "navlink">Home</a></li>
-// <li><a href="contact.html" class= "navlink">Contact</a></li>
-// </ul>`;
+    let inputAddress = address.value.trim();
+    addressAlert.innerHTML = "";
+    if(inputAddress.length < 25){
+        addressAlert.innerHTML += "Must be atleast 25 letters";
+    }
 
-// const submitted = document.querySelector("#submitted")
-// const form = document.querySelector("#form");
+    let inputEmail = email.value.trim();
+    emailAlert.innerHTML = "";
+    let emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(!emailPattern.test(inputEmail)) {
+        emailAlert.innerHTML += "Enter valid email";
+    }
 
-// const yourName = document.querySelector("#name");
-// const subject = document.querySelector("#subject");
-// const email = document.querySelector("#email");
-// const adress = document.querySelector("#adress");
-
-// const nameAlert = document.querySelector("#nameAlert");
-// const subjectAlert = document.querySelector("#subjectAlert");
-// const emailAlert = document.querySelector("#emailAlert");
-// const adressAlert = document.querySelector("#adressAlert");
-
-// form.addEventListener("submit", runForm);
-
-// function runForm(sub){
-//     sub.preventDefault();
-
-//     let inputName = yourName.value.trim();
-//     nameAlert.innerHTML = "";
-//     if (inputName.length < 2){
-//         nameAlert.innerHTML += "This name is to short";
-//     }
-//     if(/\d/.test(inputName)){
-//         nameAlert.innerHTML += "Don't use digits";
-//     }
-
-//     let inputSubject = subject.value.trim();
-//     subjectAlert.innerHTML = "";
-//     if(inputSubject.length < 10){
-//         subjectAlert.innerHTML += "Must be atleast 10 letters";
-//     }
-
-//     let inputAdress = adress.value.trim();
-//     adressAlert.innerHTML = "";
-//     if(inputAdress.length < 25){
-//         adressAlert.innerHTML += "Must be atleast 25 letters";
-//     }
-
-//     let inputEmail = email.value.trim();
-//     emailAlert.innerHTML = "";
-//     let emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//     if(!emailPattern.test(inputEmail)) {
-//         emailAlert.innerHTML += "Enter valid email";
-//     }
-
-//     if (nameAlert.innerHTML === "" && emailAlert.innerHTML === "" && subjectAlert.innerHTML === "" && adressAlert.innerHTML === "") {
-//         submitted.innerHTML = "The form har been submitted";
-//         form.submit();
-//       } else {
-//         submitted.innerHTML = "You need to fix some errors";
-//       }
-// };
+    if (nameAlert.innerHTML === "" && emailAlert.innerHTML === "" && subjectAlert.innerHTML === "" && addressAlert.innerHTML === "") {
+        submitted.innerHTML = "The form har been submitted";
+        form.submit();
+      } else {
+        submitted.innerHTML = "You need to fix some errors";
+      }
+};
 
