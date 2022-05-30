814a94d17abc2c4d5523ac408d826cb8897598a7
Lasse96 <91594348+Lasse96@users.noreply.github.com>
2022-05-30 15:32:06 +0200

    .

--- JS/contact.js
+++ JS/contact.js
@@ -1,51 +1,10 @@
-const output = document.getElementById("carousel");
-const url = "https://lassestrand.no/eksamen1/wp-json/wp/v2/posts?_embed";
 
+const submitted = document.querySelector("#submitted")
+const form = document.querySelector("#form");
 
-fetch(url)
-.then (respons => respons.json())
-.then (data => listPosts(data))
-.catch ((error) => {
-    console.error('Error', error);
-});
-
-
-function listPosts(posts){
-    let list = "";
-    console.log(posts);
-    for (let post of posts) {
-        list += `
-        <div class="post">
-        <a href="./blog.html?id=${post.id}"><img src="${post._embedded['wp:featuredmedia'][0].source_url}" class="imgposts postimg"></a>
-        <h2 class="title lesom"><a href="./blog.html?id=${post.id}" class="decor">${post.title.rendered}</a></h2>
-        </div>`;
-    }
-    output.innerHTML = list;
-}
-
-carousel = document.getElementById("carousel");
-
-function left() {
-  carousel.scrollBy({
-    top: 0,
-    left: -1020,
-    behavior: 'smooth'
-  });
-}
-function right() {
-  carousel.scrollBy({
-    top: 0,
-    left: 1020,
-    behavior: 'smooth'
-  });
-}
-
-const submitted = document.querySelector("#submitted");
-const form = document.querySelector(".form");
-
-const yourName = document.querySelector("#namee");
-const subject = document.querySelector("#subjectt");
-const email = document.querySelector("#emaill");
+const yourName = document.querySelector("#name");
+const subject = document.querySelector("#subject");
+const email = document.querySelector("#email");
 const message = document.querySelector("#message");
 
 const nameAlert = document.querySelector("#nameAlert");
@@ -53,14 +12,14 @@ const subjectAlert = document.querySelector("#subjectAlert");
 const emailAlert = document.querySelector("#emailAlert");
 const messageAlert = document.querySelector("#messageAlert");
 
-form.addEventListener("submitt", runForm);
+form.addEventListener("submit", runForm);
 
 function runForm(sub){
     sub.preventDefault();
 
     let inputName = yourName.value.trim();
     nameAlert.innerHTML = "";
-    if (inputName.length < 2){
+    if (inputName.length < 5){
         nameAlert.innerHTML += "This name is to short";
     }
     if(/\d/.test(inputName)){
@@ -73,9 +32,9 @@ function runForm(sub){
         subjectAlert.innerHTML += "Must be atleast 15 letters";
     }
 
-    let inputMessage = address.value.trim();
+    let inputmessage = message.value.trim();
     messageAlert.innerHTML = "";
-    if(inputMessage.length < 25){
+    if(inputmessage.length < 25){
         messageAlert.innerHTML += "Must be atleast 25 letters";
     }
 
@@ -92,5 +51,5 @@ function runForm(sub){
       } else {
         submitted.innerHTML = "You need to fix some errors";
       }
-}
+};
 
---
+++ JS/script.js
@@ -0,0 +1,41 @@
+const output = document.getElementById("carousel");
+const url = "https://lassestrand.no/eksamen1/wp-json/wp/v2/posts?_embed";
+
+
+fetch(url)
+.then (respons => respons.json())
+.then (data => listPosts(data))
+.catch ((error) => {
+    console.error('Error', error);
+});
+
+
+function listPosts(posts){
+    let list = "";
+    console.log(posts);
+    for (let post of posts) {
+        list += `
+        <div class="post">
+        <a href="./blog.html?id=${post.id}"><img src="${post._embedded['wp:featuredmedia'][0].source_url}" class="imgposts postimg"></a>
+        <h2 class="title lesom"><a href="./blog.html?id=${post.id}" class="decor">${post.title.rendered}</a></h2>
+        </div>`;
+    }
+    output.innerHTML = list;
+}
+
+carousel = document.getElementById("carousel");
+
+function left() {
+  carousel.scrollBy({
+    top: 0,
+    left: -1020,
+    behavior: 'smooth'
+  });
+}
+function right() {
+  carousel.scrollBy({
+    top: 0,
+    left: 1020,
+    behavior: 'smooth'
+  });
+}
